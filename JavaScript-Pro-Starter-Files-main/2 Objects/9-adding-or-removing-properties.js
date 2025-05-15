f515eded2607fef7c3f1c713e8a9102c38917205
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2025-05-14 21:24:52 -0500

    enumerating properties

--- JavaScript-Pro-Starter-Files-main/2 Objects/9-adding-or-removing-properties.js
+++ JavaScript-Pro-Starter-Files-main/2 Objects/9-adding-or-removing-properties.js
@@ -6,3 +6,28 @@ console.log(person);
 
 person.favoriteFood = "tacos";
 console.log(person);
+
+person[ "favoriteIceCream" ] = "Chocolate";
+console.log(person);
+
+delete person.favoriteIceCream;
+console.log(person);
+
+person.eat = function () {
+  console.log(`${this.name} eats ${this.favoriteFood}.`);
+};
+
+person.eat();
+
+// Lesson
+function GroceryItem(name, quantity) {
+  this.name = name;
+  this.quantity = quantity;
+  this.display = function () {
+    console.log(`${this.quantity} X ${this.name}`);
+  };
+}
+
+const newItem = new GroceryItem("Banana", 5);
+newItem.groceryAisle = "Produce";
+console.log(newItem);
